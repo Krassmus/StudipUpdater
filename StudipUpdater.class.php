722a68c7ca7f352ebaf8b8b9b7bcfab7cc4f4df5
Rasmus Fuhse <krassmus@gmail.com>
2015-04-16 07:02:01 +0200

    init plugin

--- StudipUpdater.class.php
+++ StudipUpdater.class.php
@@ -10,7 +10,25 @@ class StudipUpdater extends StudIPPlugin implements SystemPlugin {
             return;
         }
         if (stripos($_SERVER['REQUEST_URI'], "dispatch.php/start") !== false) {
-            $this->getVersions();
+            $versions = $this->getVersions();
+            $new_version = false;
+            $new_service_release = false;
+            foreach ($versions as $number => $version) {
+                if ($new_version === false && version_compare($number, $GLOBALS['SOFTWARE_VERSION'], ">")) {
+                    $new_version = $number;
+                }
+                if (($new_service_release === false)
+                        && (substr($number, 0, -1) === substr($GLOBALS['SOFTWARE_VERSION'], 0, -1))
+                        && version_compare($number, $GLOBALS['SOFTWARE_VERSION'], ">")) {
+                    $new_service_release = $number;
+                }
+            }
+            if ($new_service_release) {
+                PageLayout::postMessage(MessageBox::info(sprintf(_("Service Release %s ist verfügbar. Bitte updaten Sie so schnell wie möglich."), $new_service_release)));
+            }
+            if ($new_version) {
+                PageLayout::postMessage(MessageBox::info(sprintf(_("Neue Stud.IP Version %s ist verfügbar."), $new_version)));
+            }
         }
     }
 
