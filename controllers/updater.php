022b68d51a90886bd66bbccff81da62cff29e482
Rasmus Fuhse <krassmus@gmail.com>
2015-04-19 21:39:12 +0200

    do the file-update and redirect to web_migrate.php

--- controllers/updater.php
+++ controllers/updater.php
@@ -16,6 +16,27 @@ class UpdaterController extends PluginController {
                 )
             );
         }
+        $max_size = min(self::parse_size(ini_get('post_max_size')), self::parse_size(ini_get('upload_max_filesize')));
+        if ($max_size < 30 * 1024 * 1024) {
+            $max_size = floor($max_size / (1024 * 1024));
+            if ($max_size < 20) {
+                PageLayout::postMessage(MessageBox::error(sprintf(_("Es dürfen nur %s MB hochgeladen werden. Das ist eventuell zuwenig, um das Update einzuspielen."), $max_size)));
+            } else {
+                PageLayout::postMessage(MessageBox::info(sprintf(_("Es dürfen nur %s MB hochgeladen werden. Das ist vermutlich zuwenig, um das Update einzuspielen."), $max_size)));
+            }
+        }
+    }
+
+    static public function parse_size($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        }
+        else {
+            return round($size);
+        }
     }
 
     public function check_action()
@@ -35,7 +56,7 @@ class UpdaterController extends PluginController {
             @unlink($dir.".zip");
         }
 
-        $entries = scandir($dir);
+        $entries = (array) @scandir($dir);
         if (count($entries) === 3) {
             foreach ($entries as $entry) {
                 if ($entry !== "." && $entry !== "..") {
@@ -49,8 +70,60 @@ class UpdaterController extends PluginController {
         if (strpos($this->release_notes, $old_release_notes) !== false) {
             $this->release_notes = substr($this->release_notes, 0, strpos($this->release_notes, $old_release_notes));
         }
+    }
 
+    public function execute_action()
+    {
+        if (Request::isPost()) {
+            //nun geht es los
+            $studip_dir = $GLOBALS['ABSOLUTE_PATH_STUDIP']."/../ChangeLog";
+            $tmp_studip = $GLOBALS['TMP_PATH']."/studip_update_version";
+            $already_copied = array(".", "..");
 
+            //config
+            foreach (scandir($tmp_studip."/config") as $file) {
+                if (!in_array($file, array(".", "..", "config.inc.php", "config_local.inc.php"))) {
+                    copy($tmp_studip."/config/".$file, $studip_dir."/config/".$file);
+                }
+            }
+            $already_copied[] = "config";
+
+            //public
+            $already_copied_public = array(".", "..", "pictures");
+            @rmdirr($studip_dir."/public/plugins_packages/core");
+            copy($tmp_studip."/public/plugins_packages/core", $studip_dir."/public/plugins_packages/core");
+            $already_copied_public[] = "plugins_packages";
+            $already_copied_public[] = ".htaccess";
+            foreach (scandir($studip_dir."/public") as $file) {
+                if (!in_array($file, $already_copied_public)) {
+                    @rmdirr($studip_dir."/public/".$file);
+                }
+            }
+            foreach (scandir($tmp_studip."/public") as $file) {
+                if (!in_array($file, $already_copied_public)) {
+                    copy($tmp_studip."/public/".$file, $studip_dir."/public/".$file);
+                }
+            }
+            $already_copied[] = "public";
+
+            //data
+            $already_copied[] = "data";
+
+            //everything else
+            foreach (scandir($studip_dir) as $file) {
+                if (!in_array($file, $already_copied)) {
+                    @rmdirr($studip_dir."/public/".$file);
+                }
+            }
+            foreach (scandir($tmp_studip) as $file) {
+                if (!in_array($file, $already_copied)) {
+                    copy($tmp_studip."/".$file, $studip_dir."/".$file);
+                }
+            }
+
+            PageLayout::postMessage(MessageBox::success(_("Programmdateien erfolgreich geupdated.")));
+            header("Location: web_migrate.php");
+        }
     }
 
     protected function notWritableFolders()
--- views/updater/check.php
+++ views/updater/check.php
@@ -6,4 +6,16 @@
         <h2><?= _("Changelog der hochgeladenen Version") ?></h2>
         <?= formatReady($release_notes) ?>
     </div>
+
+    <div>
+        <h2><?= _("Was passiert beim Klick auf den Update-Ausführen Button?") ?></h2>
+        <ul>
+            <li><?= _("Die Stud.IP PHP-Programmdateien werden ersetzt.") ?></li>
+            <li><?= _("Dann fehlen aber noch die Migrationen in der Datenbank. Sie werden sofort auf der Seite web_migrate.php landen, wo Sie die Datenbankmigration von Hand anstoßen werden. Diese Migrationen können auch etwas länger dauern. Keine Bange, das ist okay so.") ?></li>
+            <li><?= _("Danach ist Ihr System geupdated!") ?></li>
+        </ul>
+        <form action="<?= PluginEngine::getLink($plugin, array(), "updater/execute") ?>" method="post">
+            <?= \Studip\Button::create(_("Update ausführen!"), "execute_update", array('return window.confirm("'._("Wirklich das Update durchführen?").'");')) ?>
+        </form>
+    </div>
 <? endif ?>
